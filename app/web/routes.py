4a5c6d5c2bb393d9db5493cc9a32e357e5fda8e8
hedgehog <hedgehog97ivan@gmail.com>
2023-03-17 22:30:24 +0300

    Добавлен основа для тестирования аксессоров через запросы
    Добавлен запрос для тестирования

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,6 +1,8 @@
 from ..admin.routes import setup_routes as admin_setup_routes
+from ..test_requests.routes import setup_routes as test_requests_setup_routes
 from .aiohttp_extansion import Application
 
 
 def setup_routes(app: Application):
     admin_setup_routes(app)
+    test_requests_setup_routes(app)
